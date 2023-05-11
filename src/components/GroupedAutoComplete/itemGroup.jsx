b296f5d04d3922e89ceb5d91507025a8657cbcd9
StAl15 <stihinaleksander@yandex.ru>
2023-05-11 22:47:32 +0500

    fix

--- src/components/GroupedAutoComplete/itemGroup.jsx
+++ src/components/GroupedAutoComplete/itemGroup.jsx
@@ -5,13 +5,14 @@ export const ItemGroup = ({params}) => {
     return (
         <div>
             <div tw={"p-2 bg-bgBeige"}>
-                <span tw={"font-semibold text-graySmText"}>{params.group.toString()}</span>
+                <span tw={"font-semibold text-graySmText"}>
+                    {params.group.toString()}
+                </span>
             </div>
             {params.children.map((item, idx) =>
                 <div
                     key={item.id}
-                    css={[idx === 0 && tw`border-l-red-600 border-l-4`,
-                        tw`p-1 pl-2 m-2 h-fit hover:bg-bgWhite hover:text-blueText`]}>
+                    css={[idx === 0 && tw`border-l-red-600 border-l-4`, tw`p-1 pl-2 m-2 h-fit hover:bg-bgWhite hover:text-blueText`]}>
                     {item}
                 </div>
             )}
